94312c013998fa6986dd03b87a1e58e7151a2566
ryukzX <mqstmr@gmail.com>
2023-05-27 14:41:03 +0300

    .

---
+++ src/pages/error404/ErrorPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Error 404 Page</h1>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ const WelcomePage = () => import("../pages/welcome/WelcomePage.vue");
 const HomePage = () => import("../pages/home/HomePage.vue");
 const LoginPage = () => import("../pages/auth/login/LoginPage.vue");
 const SignupPage = () => import("../pages/auth/signup/SignupPage.vue");
+const ErrorPage = () => import("../pages/error404/ErrorPage.vue");
 
 const router = createRouter({
   history: createWebHistory(),
@@ -12,6 +13,7 @@ const router = createRouter({
     { path: "/home", component: HomePage },
     { path: "/auth/login", component: LoginPage },
     { path: "/auth/signup", component: SignupPage },
+    { path: '/:pathMatch(.*)*', component: ErrorPage },
   ],
 });
 
